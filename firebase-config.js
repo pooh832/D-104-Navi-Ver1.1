613d2e0252689afbf8a78702da67a3f36cf4bf72
pooh832 <pooh832honeyhunt@gmail.com>
2026-03-13 00:29:03 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAU_CjOafyVtoJK9t29b-reMlFuCvoB0nk",
@@ -7,10 +8,10 @@ const firebaseConfig = {
   projectId: "d104-navi",
   storageBucket: "d104-navi.firebasestorage.app",
   messagingSenderId: "967406609529",
-  appId: "1:967406609529:web:a6e7b06550f6071a33e0a8"
+  appId: "1:967406609529:web:a6e7b06550f6071a33e0a8",
+  databaseURL: "https://d104-navi-default-rtdb.firebaseio.com"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { auth };
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
