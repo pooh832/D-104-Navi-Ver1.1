23490805fd04a3c2e07b5c923cbfc0417a823944
pooh832 <pooh832honeyhunt@gmail.com>
2026-03-13 10:36:45 +0900

    Update questions-db.js

--- questions-db.js
+++ questions-db.js
@@ -2,7 +2,8 @@ import { db } from "./firebase-config.js";
 import {
   ref,
   push,
-  onValue
+  onValue,
+  update
 } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 
 const questionsRef = ref(db, "questions");
@@ -14,7 +15,16 @@ export function addQuestion(question) {
 export function listenQuestions(callback) {
   onValue(questionsRef, (snapshot) => {
     const data = snapshot.val();
-    const questions = data ? Object.values(data) : [];
+    const questions = data
+      ? Object.entries(data).map(([id, value]) => ({ id, ...value }))
+      : [];
     callback(questions);
+  });
+}
+
+export function answerQuestion(id, answer) {
+  update(ref(db, `questions/${id}`), {
+    answer,
+    answeredAt: new Date().toISOString()
   });
 }
