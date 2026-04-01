9286eb82e9ce8fe9278b83cb1c7fbb637da7c933
pooh832 <pooh832honeyhunt@gmail.com>
2026-04-02 00:45:39 +0900

    Update events-db.js

--- events-db.js
+++ events-db.js
@@ -2,39 +2,27 @@ import { db } from "./firebase-config.js";
 import {
   ref,
   push,
-  onValue,
-  set
+  onValue
 } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 
 export function addEvent(event) {
-
   const eventsRef = ref(db, "events");
-
   push(eventsRef, event);
-
 }
 
-
 export function listenEvents(callback) {
-
   const eventsRef = ref(db, "events");
 
   onValue(eventsRef, (snapshot) => {
-
     const data = snapshot.val();
-
     const events = [];
 
-    for (const id in data) {
-      events.push(data[id]);
+    if (data) {
+      for (const id in data) {
+        events.push(data[id]);
+      }
     }
 
     callback(events);
-
   });
-
 }
-
-// disneyEvents を作成（1回だけ実行）
-const disneyRef = ref(db, "disneyEvents");
-set(disneyRef, {});
