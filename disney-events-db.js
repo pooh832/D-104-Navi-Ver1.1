968cf9e0a35820e7c327156814dc4dec7f8f9f09
pooh832 <pooh832honeyhunt@gmail.com>
2026-04-02 01:29:15 +0900

    Create disney-events-db.js

---
+++ disney-events-db.js
@@ -0,0 +1,22 @@
+import { db } from "./firebase-config.js";
+import {
+  ref,
+  onValue
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
+
+export function listenDisneyEvents(callback) {
+  const disneyRef = ref(db, "disneyEvents");
+
+  onValue(disneyRef, (snapshot) => {
+    const data = snapshot.val();
+    const disneyEvents = [];
+
+    if (data) {
+      for (const id in data) {
+        disneyEvents.push(data[id]);
+      }
+    }
+
+    callback(disneyEvents);
+  });
+}
